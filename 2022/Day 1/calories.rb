be01766924e298e4fcb5d4fef01399708abff950
Chelsea <croston@litmus.com>
2022-12-01 10:26:40 -0500

    adding part 2 solution

--- 2022/Day 1/calories.rb
+++ 2022/Day 1/calories.rb
@@ -3,3 +3,5 @@ file = File.read("2022/Day 1/elf_calories.txt").split("\n\n")
 elven_calories = file.map { |elf| elf.split("\n").map(&:to_i).sum }
 
 puts elven_calories.max
+
+puts elven_calories.max(3).sum
